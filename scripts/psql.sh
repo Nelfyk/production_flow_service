57689b80805f7c84c4f56cb72ec4c9f80e52f3da
Emperor of Russia King of Congress Poland Grand Duke of Finland <admin@example.com>
2023-08-21 15:34:09 +0000

    [develop] shit

--- scripts/psql.sh
+++ scripts/psql.sh
@@ -1,5 +1,5 @@
 docker stop production_flow_service_postgresql
 docker rm production_flow_service_postgresql
 sudo docker build -f Dockerfile_postgres -t production_flow_service_postgresql .
-docker run -p 20004:5432 production_flow_service_postgresql
+docker run -d -p 20004:5432 -v '/etc/production_flow_service/database:/var/lib/postgresql/data' production_flow_service_postgresql
 
